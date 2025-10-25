454c6c6fd2b111d22078bbc11a233a9e95839112
Cosmo Grant <cosmodgrant@gmail.com>
2025-10-25 21:54:15 +0100

    refactor: rm Questions class (unnecessary abstraction)

--- src/check_snippets.py
+++ src/check_snippets.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from typing import Literal
 
 from question import Question, Tag
-from repository import AnkiRepository, DirectoryRepository, Repository
+from repository import AnkiRepository, DirectoryRepository
 
 
 def get_user_input() -> Literal["REPLACE", "IGNORE", "MOVE ON"]:
@@ -18,101 +18,47 @@ def get_user_input() -> Literal["REPLACE", "IGNORE", "MOVE ON"]:
         return "MOVE ON"
 
 
-class Questions:
-    def __init__(self, target: str, repository: Repository):
-        self.failed: list[Question] = []
-        self.fixed: list[Question] = []
-        self.ignored: list[Question] = []
-        self.repository = repository
-        self.questions = repository.get(target)
-
-    def fix_output(self, question: Question) -> None:
-        self.repository.fix_output(question)
-        self.fixed.append(question)
-
-    def fix_formatting(self, question: Question) -> None:
-        self.repository.fix_formatting(question)
-        self.fixed.append(question)
-
-    def no_check_formatting(self, question: Question) -> None:
-        self.repository.add_tag(question, Tag.NO_CHECK_FORMATTING)
-        self.ignored.append(question)
-
-    def no_check_output(self, question: Question) -> None:
-        self.repository.add_tag(question, Tag.NO_CHECK_OUTPUT)
-        self.ignored.append(question)
-
-    def check_output(self, interactive: bool) -> None:
-        questions_to_check = [question for question in self.questions if question.check_output]
-        print("----------")
-
-        for question in questions_to_check:
-            if not question.has_ok_output():
-                print(f"\N{CROSS MARK} Unexpected output for {question.id}.", end="\n\n")
-                print("Code:")
-                colour_print(question.snippet.code, colour="cyan", end="\n\n")
-                print("Output (normalised):")
-                colour_print(question.snippet.output.normalised, colour="green", end="\n\n")
-                print("Given (normalised):")
-                colour_print(question.output.normalised, colour="red", end="\n\n")
-                self.failed.append(question)
-                if interactive:
-                    response = get_user_input()
-                    if response == "REPLACE":
-                        self.fix_output(question)
-                        print("\N{SPARKLES} Replaced.", end="\n\n")
-                    elif response == "IGNORE":
-                        self.no_check_output(question)
-                        print("\N{SEE-NO-EVIL MONKEY} Will ignore in future.", end="\n\n")
-                    else:
-                        print("\N{FACE WITHOUT MOUTH} Moving on.", end="\n\n")
-                print("----------", end="\n\n")
-
-    def check_formatting(self, interactive: bool) -> None:
-        questions_to_check = [question for question in self.questions if question.check_formatting]
-        print("----------")
-
-        for question in questions_to_check:
-            formatted = question.snippet.format(compressed=isinstance(self.repository, AnkiRepository))  # TODO: hack
-            if formatted is None:
-                # error when trying to format snippet
-                # treat as non-fixable failure
-                print(f"\N{CROSS MARK} Error when formatting {question.id}.", end="\n\n")
-                print("Given:")
-                colour_print(question.snippet.code, colour="red")
-                self.failed.append(question)
-                print("----------")
-            elif formatted != question.snippet.code:
-                print(f"\N{CROSS MARK} Unexpected formatting for {question.id}.", end="\n\n")
-                print("Formatted:")
-                colour_print(formatted, colour="green", end="\n\n")
-                print("Given:")
-                colour_print(question.snippet.code, colour="red")
-                self.failed.append(question)
-                if interactive:
-                    response = get_user_input()
-                    if response == "REPLACE":
-                        self.fix_formatting(question)
-                        print("\N{SPARKLES} Replaced.")
-                    elif response == "IGNORE":
-                        self.no_check_formatting(question)
-                        print("\N{SEE-NO-EVIL MONKEY} Will ignore in future.")
-                    else:
-                        print("\N{FACE WITHOUT MOUTH} Moving on.")
-                print("----------")
-
-
 def check_output(args) -> int:
     repository = DirectoryRepository() if Path(args.target).is_dir() else AnkiRepository()
-    questions = Questions(target=args.target, repository=repository)
-    questions.check_output(args.interactive)
-    if questions.failed:
+    failed: list[Question] = []
+    fixed: list[Question] = []
+    ignored: list[Question] = []
+    repository = repository
+    questions = repository.get(args.target)
+    questions_to_check = [question for question in questions if question.check_output]
+    print("----------")
+
+    for question in questions_to_check:
+        if not question.has_ok_output():
+            print(f"\N{CROSS MARK} Unexpected output for {question.id}.", end="\n\n")
+            print("Code:")
+            colour_print(question.snippet.code, colour="cyan", end="\n\n")
+            print("Output (normalised):")
+            colour_print(question.snippet.output.normalised, colour="green", end="\n\n")
+            print("Given (normalised):")
+            colour_print(question.output.normalised, colour="red", end="\n\n")
+            failed.append(question)
+            if args.interactive:
+                response = get_user_input()
+                if response == "REPLACE":
+                    repository.fix_output(question)
+                    fixed.append(question)
+                    print("\N{SPARKLES} Replaced.", end="\n\n")
+                elif response == "IGNORE":
+                    repository.add_tag(question, Tag.NO_CHECK_OUTPUT)
+                    ignored.append(question)
+                    print("\N{SEE-NO-EVIL MONKEY} Will ignore in future.", end="\n\n")
+                else:
+                    print("\N{FACE WITHOUT MOUTH} Moving on.", end="\n\n")
+            print("----------", end="\n\n")
+
+    if failed:
         print(
-            f"{len(questions.failed)} questions had bad output "
+            f"{len(failed)} questions had bad output "
             "("
-            f"{len(questions.fixed)} fixed, "
-            f"{len(questions.ignored)} will be ignored in future, "
-            f"{len(questions.failed) - len(questions.fixed) - len(questions.ignored)} remaining"
+            f"{len(fixed)} fixed, "
+            f"{len(ignored)} will be ignored in future, "
+            f"{len(failed) - len(fixed) - len(ignored)} remaining"
             ")"
         )
         return 1
@@ -123,15 +69,52 @@ def check_output(args) -> int:
 
 def check_formatting(args) -> int:
     repository = DirectoryRepository() if Path(args.target).is_dir() else AnkiRepository()
-    questions = Questions(target=args.target, repository=repository)
-    questions.check_formatting(args.interactive)
-    if questions.failed:
+    failed: list[Question] = []
+    fixed: list[Question] = []
+    ignored: list[Question] = []
+    repository = repository
+    questions = repository.get(args.target)
+    questions_to_check = [question for question in questions if question.check_formatting]
+    print("----------")
+
+    for question in questions_to_check:
+        formatted = question.snippet.format(compressed=isinstance(repository, AnkiRepository))  # TODO: hack
+        if formatted is None:
+            # error when trying to format snippet
+            # treat as non-fixable failure
+            print(f"\N{CROSS MARK} Error when formatting {question.id}.", end="\n\n")
+            print("Given:")
+            colour_print(question.snippet.code, colour="red")
+            failed.append(question)
+            print("----------")
+        elif formatted != question.snippet.code:
+            print(f"\N{CROSS MARK} Unexpected formatting for {question.id}.", end="\n\n")
+            print("Formatted:")
+            colour_print(formatted, colour="green", end="\n\n")
+            print("Given:")
+            colour_print(question.snippet.code, colour="red")
+            failed.append(question)
+            if args.interactive:
+                response = get_user_input()
+                if response == "REPLACE":
+                    repository.fix_formatting(question)
+                    fixed.append(question)
+                    print("\N{SPARKLES} Replaced.")
+                elif response == "IGNORE":
+                    repository.add_tag(question, Tag.NO_CHECK_FORMATTING)
+                    ignored.append(question)
+                    print("\N{SEE-NO-EVIL MONKEY} Will ignore in future.")
+                else:
+                    print("\N{FACE WITHOUT MOUTH} Moving on.")
+            print("----------")
+
+    if failed:
         print(
-            f"{len(questions.failed)} questions had bad formatting "
+            f"{len(failed)} questions had bad formatting "
             "("
-            f"{len(questions.fixed)} fixed, "
-            f"{len(questions.ignored)} will be ignored in future, "
-            f"{len(questions.failed) - len(questions.fixed) - len(questions.ignored)} left"
+            f"{len(fixed)} fixed, "
+            f"{len(ignored)} will be ignored in future, "
+            f"{len(failed) - len(fixed) - len(ignored)} left"
             ")"
         )
         return 1
