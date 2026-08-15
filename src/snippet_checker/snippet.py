009fb56a5b44f95aeb2f442bbe164a80d107cf60
Cosmo Grant <cosmodgrant@gmail.com>
2026-08-15 21:15:10 +0100

    close output stream on linux but not mac

--- src/snippet_checker/snippet.py
+++ src/snippet_checker/snippet.py
@@ -149,11 +149,14 @@ class DockerExecutor:
             # See #30.
             container.restart()
 
-        output_stream.close()
-        # CancellableStream.close() doesn't close the underlying response,
-        # causing "ValueError: I/O operation on closed file" at shutdown.
-        # See https://github.com/docker/docker-py/issues/3345
-        output_stream._response.close()
+        # TODO: the close() calls are required on linux but cause errors on mac.
+        # I'd like to understand this better.
+        if platform.system() == "Linux":
+            output_stream.close()
+            # CancellableStream.close() doesn't close the underlying response,
+            # causing "ValueError: I/O operation on closed file" at shutdown.
+            # See https://github.com/docker/docker-py/issues/3345
+            output_stream._response.close()
 
         return to_string(logs, hanged)
 
